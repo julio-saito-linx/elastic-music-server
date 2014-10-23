9296c7cda9cbe2a72417acc1fab9485a8856ba5d
Julio Makdisse Saito <saitodisse@gmail.com>
2014-10-23 21:50:15 -0200

    just serves the files making work with AZK

--- app.js
+++ app.js
@@ -1,78 +1,33 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
+var static = require('node-static');
+var fs = require('fs');
 var path = require('path');
 
-var app = express();
+//
+// Create a node-static server to serve the current directory
+//
+var fullPath = '../../mounts';
+console.log('fs.existsSync(fullPath) = ', fs.existsSync(fullPath));
+var file = new static.Server(fullPath, { cache: 7200, headers: {'X-Hello':'World!'} });
 
-// all environments
-app.set('port', process.env.PORT || 9004);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-
-app.get('/go/*', function(req, res, next) {
-  console.log('req.route.method:', req.route.method, req.url);
-  var newUrl = transformUrl(req.url);
-  res.redirect(newUrl);
+require('http').createServer(function (request, response) {
+  // response.redirect(newUrl);
 
   // CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  // response.header("Access-Control-Allow-Origin", "*");
+  // response.header("Access-Control-Allow-Headers", "X-Requested-With");
 
-  next();
-});
+    file.serve(request, response, function (err, res) {
 
-function transformUrl(oldPath) {
-  // replace "/go/*"  to  "/*"
-  oldPath = oldPath.substring(3);
-  oldPath = decodeURIComponent(oldPath);
-  
-  if(oldPath.indexOf('/media/julio/4 H-MP3 (1,36 TB)/') >= 0){
-    return oldPath.replace('/media/julio/4 H-MP3 (1,36 TB)/', '/MP3-01/');
-  }
-  if(oldPath.indexOf('/media/julio/B21AB1E71AB1A92D/') >= 0){
-    return oldPath.replace('/media/julio/B21AB1E71AB1A92D/', '/MP3-02/');
-  }
-  if(oldPath.indexOf('/media/julio/2GB, new/') >= 0){
-    return oldPath.replace('/media/julio/2GB, new/', '/MP3-03/');
-  }
-  if(oldPath.indexOf('/media/julio/Files/') >= 0){
-    return oldPath.replace('/media/julio/Files/', '/MP3-04/');
-  }
-  if(oldPath.indexOf('/home/julio/Música/') >= 0){
-    return oldPath.replace('/home/julio/Música/', '/MP3-05/');
-  }
-};
+        if (err) { // An error as occured
+            console.error("> Error serving " + request.url + " - " + err.message);
+            response.writeHead(err.status, err.headers);
+            response.end();
+        } else { // The file was served successfully
+            console.log("> " + request.url + " - " + res.message);
 
+        }
+    });
+}).listen(process.env.HTTP_PORT || 9004);
 
-// static local files
-// TODO: this must be configurable
-app.use('/MP3-01', express.static( '/media/julio/4 H-MP3 (1,36 TB)/' ));
-app.use('/MP3-02', express.static( '/media/julio/B21AB1E71AB1A92D/' ));
-app.use('/MP3-03', express.static( '/media/julio/2GB, new/' ));
-app.use('/MP3-04', express.static( '/media/julio/Files/' ));
-app.use('/MP3-05', express.static( '/home/julio/Música/' ));
+console.log("> node-static is listening...");
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
